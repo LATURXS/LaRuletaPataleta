2349302b01b971e100cf280fb7493a623ffdf3e5
LATURXS <119787110+LATURXS@users.noreply.github.com>
2025-07-18 21:42:43 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>¡Ya estamos en el juego de La Ruleta pataleta!</h1>
+      <h1>¡Ya estamos en el juego de La ruleta pataleta!</h1>
       <p>Esta es la versión web del juego.</p>
     </div>
   );
